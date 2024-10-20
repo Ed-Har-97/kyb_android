f879b75222557457c97b488a97fcb1851c3a2f95
EduardHarutyunyan97 <eduard97harutyunyan@gmail.com>
2024-10-20 15:36:03 +0300

    company information screen

---
+++ app/src/main/java/com/example/bochackathon_android/ui/company_documents/CompanyDocumentsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.bochackathon_android.ui.company_documents
+
+import com.example.bochackathon_android.R
+import com.example.bochackathon_android.base.BaseFragment
+
+class CompanyDocumentsFragment : BaseFragment(R.layout.fragment_company_documents)
--- app/src/main/java/com/example/bochackathon_android/ui/company_information/CompanyInformationFragment.kt
+++ app/src/main/java/com/example/bochackathon_android/ui/company_information/CompanyInformationFragment.kt
@@ -1,8 +1,23 @@
 package com.example.bochackathon_android.ui.company_information
 
+import android.os.Bundle
+import android.view.View
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.bochackathon_android.R
 import com.example.bochackathon_android.base.BaseFragment
+import com.example.bochackathon_android.databinding.FragmentCompanyInformationBinding
 
 class CompanyInformationFragment : BaseFragment(R.layout.fragment_company_information) {
+    private val binding: FragmentCompanyInformationBinding by viewBinding(
+        FragmentCompanyInformationBinding::bind
+    )
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews(){
+        binding.nextBtn.isEnabled=false // incompleted flow
+    }
 }
